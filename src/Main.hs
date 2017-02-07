cc4788d6a921fb6ca1affff42b4eee483ec20962
Johan Andersson <johan@localhost.localdomain>
2017-02-07 20:49:53 +0100

    refactor

--- src/Main.hs
+++ src/Main.hs
@@ -3,17 +3,30 @@ import Data.Binary.Get
 import Data.Word
 import Data.Int
  
-deserialiseHeader :: Get (BL.ByteString,BL.ByteString, Word32)
+deserialiseHeader :: Get (BL.ByteString,Word8, Word8)
 deserialiseHeader = do
-  len <- getWord8
-  version <- getLazyByteString $ word8ToInt64 len 
-  plen <- getLazyByteStringNul
-  chksum <- getWord32be
-  return (version,plen, chksum)
+  version <- readBytes 30
+  len2 <- getWord8
+  what <- getWord8
+  return (version,what, len2)
+
+readBytes max = do
+    len <- getWord8
+    str <- readBytesLen max $ fromIntegral len
+    return str
+
+readBytesLen :: Int -> Int -> Get (BL.ByteString)
+readBytesLen max len = do
+    str <- getLazyByteString $ fromIntegral len
+    ignore <- skip $ max - len
+    return str
+
+read2 :: Int64 -> Int64 -> Get BL.ByteString
+read2 max len = getLazyByteString len 
 
 
 word8ToInt64 :: Word8 -> Int64
-word8ToInt64  w = fromIntegral w
+word8ToInt64  = fromIntegral 
 
  
 main :: IO ()
